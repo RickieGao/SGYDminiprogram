30f2a2767612aa4a3c9b36a2c286a2215f79b429
Rickie Gao <Gao_zh@foxmail.com>
2021-09-28 02:13:55 +0800

    vision 0.6

--- pages/world/world.js
+++ pages/world/world.js
@@ -1,11 +1,15 @@
 // pages/world/world.js
+
+// const db = wx.cloud.database();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // newsList:[],
+    // openid:'',
   },
 
   /**
@@ -13,7 +17,70 @@ Page({
    */
   onLoad: function (options) {
 
+    // setTimeout(()=>{
+    //   this.setData({
+    //     className:"content active"
+    //   });      
+    // },2000);
+    
+    // this.setData({
+    //   openid:wx.getStorageSync('openid')
+    // });
+    // this.getData();
   },
+
+  // getData(){
+    
+    // db.collection('card').where({
+    //   _openid: this.data.openid
+    // }).get({
+    //   success:res=>{
+      
+    //     let data = res.data.map((item)=>{
+    //         item.className = 'content';
+    //         return item;
+    //     });
+
+    //     this.setData({
+    //       newsList:data
+    //     });
+    //   }
+    // })
+  // },
+
+  // remove(e){
+  //   console.log(e.target.dataset.id);
+  //   let id = e.target.dataset.id;
+  //   let list = this.data.newsList;
+  //   let changeList = list.map((item)=>{
+  //       if(item._id === id){
+  //         item.className = 'content active';
+  //       }
+  //       return item;
+  //   });
+
+    //TODO:加上删除动画
+    /*
+      修改active这个class  
+        加上动画，css animation
+
+    */
+
+    // this.setData({
+    //   newsList:changeList
+    // });
+    
+    // db.collection('card').doc(id).remove({
+    //   success:(res)=>{
+    //     console.log(res);
+    //     this.getData();
+    //   }
+    // });
+  // },
+
+  /*
+    删除做成动画
+  */
 
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -46,21 +113,21 @@ Page({
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  // onPullDownRefresh: function () {
+  //   this.getData();
+  // },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  // onReachBottom: function () {
 
-  },
+  // },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  // onShareAppMessage: function () {
 
-  }
+  // }
 })
